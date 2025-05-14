e92b01dd4faa7c300cc013bfbadc71cbde067895
Mohammed Yassin <mohammedyassin306@gmail.com>
2025-05-15 01:07:12 +0300

    added levels system

--- Assets/Assets/Scripts/You Win.cs
+++ Assets/Assets/Scripts/You Win.cs
@@ -6,6 +6,18 @@ public class YouWin : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        unlockNewLevel();
         GameManager.instance.PlayerFinishedLevel();
+    }
+
+    void unlockNewLevel(){
+
+    if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+    {
+    PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+    PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+    PlayerPrefs.Save();
+    
+    }
     }
 }
